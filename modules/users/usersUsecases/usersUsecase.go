c6cbf9f4ff615fb489b48cd67c191fe2a6fad5be
chok <idchokms01@outlook.com>
2023-10-01 16:33:42 +0700

    -- feature/users :: signin

--- modules/users/usersUsecases/usersUsecase.go
+++ modules/users/usersUsecases/usersUsecase.go
@@ -1,13 +1,18 @@
 package usersUsecases
 
 import (
+	"fmt"
+
 	"github.com/codepnw/ecommerce/config"
 	"github.com/codepnw/ecommerce/modules/users"
 	"github.com/codepnw/ecommerce/modules/users/usersRepositories"
+	"github.com/codepnw/ecommerce/pkg/auth"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type IUsersUsecase interface {
 	InsertCustomer(req *users.UserRegisterReq) (*users.UserPassport, error)
+	GetPassport(req *users.UserCredential) (*users.UserPassport, error)
 }
 
 type usersUsecase struct {
@@ -33,3 +38,45 @@ func (u *usersUsecase) InsertCustomer(req *users.UserRegisterReq) (*users.UserPa
 	}
 	return result, nil
 }
+
+func (u *usersUsecase) GetPassport(req *users.UserCredential) (*users.UserPassport, error) {
+	user, err := u.repository.FindOneUserByEmail(req.Email)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+		return nil, fmt.Errorf("password is invalid")
+	}
+
+	accessToken, err := auth.NewEcomAuth(auth.Access, u.cfg.Jwt(), &users.UserClaims{
+		Id: user.Id,
+		RoleId: user.RoleId,
+	})
+
+	refreshToken, err := auth.NewEcomAuth(auth.Refresh, u.cfg.Jwt(), &users.UserClaims{
+		Id: user.Id,
+		RoleId: user.RoleId,
+	})
+
+	passport := &users.UserPassport{
+		User: &users.User{
+			Id: user.Id,
+			Email: user.Email,
+			Username: user.Username,
+			RoleId: user.RoleId,
+		},
+		Token: &users.UserToken{
+			AccessToken: accessToken.SignToken(),
+			RefreshToken: refreshToken.SignToken(),
+		},
+	}
+
+	if err := u.repository.InsertOauth(passport); err != nil {
+		return nil, err
+	}
+	return passport, nil
+}
+
+
+
