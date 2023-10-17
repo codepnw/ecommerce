f7b7f26425ae1bdf3f883bcef041979da3bbd1f4
chok <idchokms01@outlook.com>
2023-10-17 20:33:17 +0700

    -- feature/files :: delete file

--- modules/files/filesUsecases/filesUsecase.go
+++ modules/files/filesUsecases/filesUsecase.go
@@ -14,6 +14,7 @@ import (
 
 type IFilesUsecase interface {
 	UploadToStorage(req []*files.FileReq) ([]*files.FileRes, error)
+	DeleteFileOnStorage(req []*files.DeleteFileReq) error
 }
 
 type filesUsecase struct {
